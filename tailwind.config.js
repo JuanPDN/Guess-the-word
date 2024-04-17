0fba7c42499310c03a0dec4172edb464f1759d91
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-04-17 10:55:12 -0500

    finished design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,22 @@
 module.exports = {
   content: ["./*.html", "./src/js/*.js"],
   theme: {
-    colors: {
-      "7429C6": "#7429C6",
-      C951E7: "#C951E7",
-      672171: "#672171",
-      "4A5567": "#4A5567",
-      "97A3B6": "#97A3B6",
-      "030616": "#030616",
-      F2F5F9: "#F2F5F9",
-    },
-    backgroundImage: {
-      guess: 'url("../assets/bg-guess-the-word.png")',
-    },
     extend: {
       fontFamily: {
         Outfit: ['"Outfit"', "sans-serif"],
+      },
+      backgroundImage: {
+        guess: 'url("../assets/bg-guess-the-word.png")',
+      },
+      colors: {
+        "7429C6": "#7429C6",
+        C951E7: "#C951E7",
+        672171: "#672171",
+        "4A5567": "#4A5567",
+        "97A3B6": "#97A3B6",
+        "030616": "#030616",
+        F2F5F9: "#F2F5F9",
+        "20293A": "#20293A",
       },
     },
   },
